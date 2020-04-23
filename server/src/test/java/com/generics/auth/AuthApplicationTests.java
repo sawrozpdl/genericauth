f8b06a191244521f608c9305354cbb2517afe7f1
sawrozpdl <sarojpaudyal53@gmail.com>
2020-04-23 20:39:44 +0545

    add tests

--- server/src/test/java/com/generics/auth/AuthApplicationTests.java
+++ server/src/test/java/com/generics/auth/AuthApplicationTests.java
@@ -1,9 +1,12 @@
 package com.generics.auth;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@DataJpaTest
 class AuthApplicationTests {
 
 	@Test
