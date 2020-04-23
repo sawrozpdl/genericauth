7246bb64e516455cac429fa2e955d1a33cbd915e
sawrozpdl <sarojpaudyal53@gmail.com>
2020-04-23 11:46:23 +0545

    hotfix env

--- server/src/main/java/com/generics/auth/controller/RootController.java
+++ server/src/main/java/com/generics/auth/controller/RootController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    @Value("${server.env}")
+    @Value("${spring.env}")
     String env;
 
     @GetMapping("/api")
